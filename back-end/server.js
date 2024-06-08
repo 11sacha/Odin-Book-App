e2e41ad86d4a4ab682ad63e41bfd838b7e9622cb
sacha11 <s.j.g_97@hotmail.com>
2024-06-08 13:27:59 -0300

    updating register route

--- back-end/server.js
+++ back-end/server.js
@@ -8,7 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { register } from './controllers/auth.js';
+import { register } from "./controllers/auth.js"
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -38,6 +38,8 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+//
 
 // MONGOOSE SET UP
 const PORT = process.env.PORT || 6000;
