740d73429a5ca6e7e8a8bda14b7c47d719625795
sacha11 <s.j.g_97@hotmail.com>
2024-06-08 11:59:25 -0300

    added user model

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -7,6 +7,40 @@ const UserSchema = new Schema(
             required: true,
             min: 2,
             max: 50,
-        }
-    }
-)
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            max: 50,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+        },
+        picturePath: {
+            type: String,
+            default: ""
+        },
+        friends: {
+            type: Array,
+            default: []
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impressions: Number,
+    },
+    { timestamps: true }
+);
+
+const User = model("User", UserSchema);
+
+export default User;
