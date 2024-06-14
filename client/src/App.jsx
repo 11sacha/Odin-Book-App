480d687da74e99f2c7fe01fbca1e9930a3ecaf61
sacha11 <s.j.g_97@hotmail.com>
2024-06-14 16:03:04 -0300

    App.jsx routes set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage/homePage';
+import LoginPage from 'scenes/loginPage/loginPage';
+import ProfilePage from 'scenes/profilePage/profilePage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='app'>
-      hello
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <LoginPage/> }/>
+          <Route path='/home' element={ <HomePage/> }/>
+          <Route path='/profile/:userId' element={ <ProfilePage/> }/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
