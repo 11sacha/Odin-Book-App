6880820683ae5678b08ce056a126f92adb818e3c
sacha11 <s.j.g_97@hotmail.com>
2024-06-15 12:58:03 -0300

    Color, theme and dark mode set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from 'scenes/homePage/homePage';
-import LoginPage from 'scenes/loginPage/loginPage';
-import ProfilePage from 'scenes/profilePage/profilePage';
+import HomePage from './scenes/homePage/homePage';
+import LoginPage from './scenes/loginPage/loginPage';
+import ProfilePage from './scenes/profilePage/profilePage';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { themeSettings } from './theme.js';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mode = useSelector((state) => state.mod);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className='app'>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={ <LoginPage/> }/>
-          <Route path='/home' element={ <HomePage/> }/>
-          <Route path='/profile/:userId' element={ <ProfilePage/> }/>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path='/' element={ <LoginPage/> }/>
+            <Route path='/home' element={ <HomePage/> }/>
+            <Route path='/profile/:userId' element={ <ProfilePage/> }/>
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   )
