42ca23d34a9bfc0c7f40d96690f44806a25e6ec3
sacha11 <s.j.g_97@hotmail.com>
2024-08-08 13:17:10 -0300

    Building homepage

--- client/src/scenes/homePage/homePage.jsx
+++ client/src/scenes/homePage/homePage.jsx
@@ -1,11 +1,24 @@
-import { Box } from '@mui/material'
+import { Box, useMediaQuery } from '@mui/material'
 import React from 'react'
 import navBar from './navBar/navBar'
+import { useSelector } from 'react-redux';
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+  
   return (
     <Box>
       <navBar />
+      <Box 
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+
+      </Box>
     </Box>
   )
 }
