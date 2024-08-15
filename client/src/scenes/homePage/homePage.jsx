ae96d5046d45ac209280b8855eeb78fd460a0883
sacha11 <s.j.g_97@hotmail.com>
2024-08-15 10:01:21 -0300

    advert widget added to homepage

--- client/src/scenes/homePage/homePage.jsx
+++ client/src/scenes/homePage/homePage.jsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import UserWidget from 'scenes/widgets/UserWidget';
 import MyPostWidget from 'scenes/widgets/MyPostWidget';
 import PostsWidget from 'scenes/widgets/PostsWidget';
+import AdvertWidget from 'scenes/widgets/AdvertWidget';
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
@@ -27,7 +28,11 @@ const HomePage = () => {
           <MyPostWidget picturePath={picturePath}/>
           <PostsWidget userId={_id}/>
         </Box>
-        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+          </Box>
+        )}
       </Box>
     </Box>
   )
